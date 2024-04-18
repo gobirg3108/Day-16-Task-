0c1cb45ba0e8d3ae042336aaeddb1dd849413281
gobirg3108 <163556882+gobirg3108@users.noreply.github.com>
2024-04-18 15:19:19 +0530

    Add files via upload

---
+++ Day - 16 Task/index.js
@@ -0,0 +1,33 @@
+//Call back hell task:
+
+var counterdownElement = document.getElementById("countdown");
+setTimeout(function () {
+    counterdownElement.textContent ="9";
+    setTimeout(function () {
+        counterdownElement.textContent ="8";
+        setTimeout(function() {
+            counterdownElement.textContent ="7";
+            setTimeout(function () {
+                counterdownElement.textContent ="6"
+                setTimeout(function() {
+                    counterdownElement.textContent ="5";
+                    setTimeout(function() {
+                        counterdownElement.textContent ="4";
+                        setTimeout(function() {
+                            counterdownElement.textContent ="3";
+                            setTimeout(function() {
+                                counterdownElement.textContent ="2";
+                                setTimeout(function(){
+                                    counterdownElement.textContent ="1";
+                                    setTimeout(function() {
+                                        counterdownElement.textContent ="Happy Independence Day !!!"
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
